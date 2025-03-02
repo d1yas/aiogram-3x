af7b258174b993e2d0637b5e5aab53c74eefe2e4
d1yas <diyasdjumabaevv@gmail.com>
2025-03-02 09:51:12 +0500

    FSM Context

--- app/handlers.py
+++ app/handlers.py
@@ -1,10 +1,16 @@
 from aiogram import types, F, Router
 from aiogram.filters import CommandStart,Command
 from aiogram.types import Message,  CallbackQuery
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
 
 router = Router()
+
+class Register(StatesGroup):
+    name = State()
+    phone = State()
 
 @router.message(CommandStart())
 async def start_command(message: types.Message):
@@ -23,4 +29,25 @@ async def inline_cars(message: types.Message):
 @router.callback_query(F.data == 'catalog')
 async def callback_query_func(call: types.CallbackQuery):
     await call.answer('Вы выбрали каталог', show_alert=True)
-    await call.message.edit_text("Callback query", reply_markup=kb.inline_cars())
+    await call.message.edit_text("Callback query", reply_markup= await kb.inline_cars())
+
+
+
+@router.message(Command("register"))
+async def register_command(message: types.Message,state: FSMContext):
+    await state.set_state(Register.name)
+    await message.answer("Введите Ваше имя ")
+
+@router.message(Register.name)
+async def register_state(message: types.Message,state: FSMContext):
+    await state.update_data(name= message.text)
+    await state.set_state(Register.phone)
+    await message.answer("Введите номер телефона! ")
+
+
+@router.message(Register.phone)
+async def register_phone(message: types.Message,state: FSMContext):
+    await state.update_data(phone= message.text)
+    data = await state.get_data()
+    await message.answer(f"Спасибо за Регистрацию\nName: {data['name']}, Phone: {data['phone']}")
+    await state.clear()
